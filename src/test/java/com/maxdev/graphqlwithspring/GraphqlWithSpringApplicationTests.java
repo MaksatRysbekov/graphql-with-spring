1f83d4f3806ed900014b6618421425818e3bda22
MaksatRysbekov <maksatrysbekov96@gmail.com>
2021-03-20 20:15:01 +0100

    first commit

---
+++ src/test/java/com/maxdev/graphqlwithspring/GraphqlWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxdev.graphqlwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
